3f6df322f01ba5a49acf39e26cbfd5cbf83a57e4
zlkca <likzhang@gmail.com>
2020-04-28 10:54:00 -0400

    Refactory to Order Table

--- src/views/Orders/OrderPage.js
+++ src/views/Orders/OrderPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import OrderListPage from "views/Orders/OrderListPage";
+import OrderTablePage from "views/Orders/OrderTablePage";
 // import OrderFormPage from "views/Orders/OrderFormPage";
 
 const OrderPage = () => {
   return (
     <Switch>
       {/* <Route exact path="/orders/:id" component={OrderFormPage} /> */}
-      <Route exact path="/orders" component={OrderListPage} />
+      <Route exact path="/orders" component={OrderTablePage} />
     </Switch>
   );
 };
