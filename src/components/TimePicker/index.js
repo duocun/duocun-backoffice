eebfcf0296719fa21b5528cca83b4ceddeb5893b
zlkca <likzhang@gmail.com>
2020-04-30 12:20:01 -0400

    change base url and history url

--- src/components/TimePicker/index.js
+++ src/components/TimePicker/index.js
@@ -3,16 +3,16 @@ import { DatePicker } from "@material-ui/pickers";
 import Moment from 'react-moment';
 import 'moment-timezone';
 
-export default function datePicker({}) {
-  const [selectedDate, handleDateChange] = useState(new Date());
+export default function datePicker() {
+  // const [selectedDate, handleDateChange] = useState(new Date());
 
   return (
     <div>
       <DatePicker
         variant="inline"
         label="Basic example"
-        value={selectedDate}
-        onChange={handleDateChange}
+        // value={selectedDate}
+        // onChange={handleDateChange}
       />
     </div>
   );
