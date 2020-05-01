f11bfb63fc5a3fb1d27b37c637b2c3fabb55e999
brianwang <brianwang2013@hotmail.com>
2020-05-01 14:39:02 -0400

    close hardcode data to real data

--- src/views/Dashboard/DriverSummaryPage.js
+++ src/views/Dashboard/DriverSummaryPage.js
@@ -66,8 +66,8 @@ const DriverSummaryPage = ({driverSummaryArray,loadDriverSummary}) => {
   };
 
   useEffect(() => {
-    // const startDate = moment().format('YYYY-MM-DD');
-    const startDate = '2020-04-03';  // hard coded date because 05-01 has no data
+    const startDate = moment().format('YYYY-MM-DD');
+    // const startDate = '2020-04-03';  // hard coded date because 05-01 has no data
     loadDriverSummary(startDate);
   }, []);
   return (
