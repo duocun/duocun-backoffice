3c06903a0ffaa6884aa5f6b07bf38fceec11794d
zlkca <likzhang@gmail.com>
2020-07-02 15:05:52 -0400

    Display 960 picture by default

--- src/helper/index.js
+++ src/helper/index.js
@@ -183,7 +183,7 @@ export const countProductFromDate = (
 
 export const getPictureUrl = src => {
   const [fname, ext] = src.split('.');
-  return `${MEDIA_PATH}/${fname}.${ext}`;
+  return `${MEDIA_PATH}/${fname}_960.${ext}`;
   // return process.env.REACT_APP_MEIDA_URL + `/${src}`;
 };
 
